bf5dd42bb5af8bd1cb06838964c0bca81aa23779
Alexandre Villela - SleX <slex@slex.com.br>
2015-09-29 22:58:47 -0300

    tests

--- tests/test_image_aspect.py
+++ tests/test_image_aspect.py
@@ -18,26 +18,32 @@
 #    by sx.slex@gmail.com
 
 import os
+# import pprint
 import unittest
-import tempfile
-from sxtools import ImageAspect
+# import tempfile
+import ImageAspect
 PROJECT_APPLICATION_PATH = os.path.dirname(os.path.abspath(__file__))
 CACHE_PATH = PROJECT_APPLICATION_PATH
 
 
 class TestImageAspect(unittest.TestCase):
 
-    def test_image_aspect_1(self):
+    def test_01_image_aspect_contents(self):
         filename_or_obj = os.path.join(CACHE_PATH, 'imgs/wallpaper.jpg')
-        ai = ImageAspect(filename_or_obj)
-        ai.aspect(aspect='distortion', width=200, height=100)
-        self.assertIn(
-            'data:image/jpeg;base64,',
-            ai.base64()
-        )
+        ai = ImageAspect.ImageAspect(filename_or_obj)
+        ai.aspect(width=200, height=100, aspect='distortion')
+        self.assertTrue(len(ai.contents()) > 2000)
 
-    def test_image_aspect_2(self):
+    def test_02_image_aspect_base64(self):
         filename_or_obj = os.path.join(CACHE_PATH, 'imgs/wallpaper.jpg')
-        ai = ImageAspect(filename_or_obj)
-        ai.aspect(aspect='distortion', width=200, height=100)
-        ai.save(os.path.join(tempfile.gettempdir(), 'filename_new.jpg'))
+        ai = ImageAspect.ImageAspect(filename_or_obj)
+        ai.aspect(width=200, height=100, aspect='distortion')
+        self.assertIn('data:image/JPEG;base64', ai.base64())
+
+    def test_03_image_aspect_save(self):
+        filename_or_obj = os.path.join(CACHE_PATH, 'imgs/wallpaper.jpg')
+        new_filename = os.path.join(CACHE_PATH, 'imgs/wallpaper_new.jpg')
+
+        ai = ImageAspect.ImageAspect(filename_or_obj)
+        ai.aspect(width=200, height=100, aspect='distortion')
+        ai.save(new_filename)
